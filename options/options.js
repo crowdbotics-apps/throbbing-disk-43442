364d5bd4014893610327cc3b56ccead51c2c316c
Crowdbotics <team@crowdbotics.com>
2023-09-18 16:46:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "throbbing-disk-43442",
+  url: "https://throbbing-disk-43442.botics.co",
+  api: "https://throbbing-disk-43442.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
